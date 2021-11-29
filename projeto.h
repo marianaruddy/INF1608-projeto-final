d8c8fb530f2084e6b9cf546268b799905d291f72
Mariana Ruddy <31486623+marianaruddy@users.noreply.github.com>
2021-11-29 09:59:40 -0300

    delete projeto.h

--- projeto.h
+++
@@ -1,3 +0,0 @@
-double newton_raphson(double x0, double f (double x), double f_derivada (double x), double tol, int num_max_iter);
-
-void calc_coord_parametricas (double x, double y, double u , double v , double dudx , double dvdx , double dudy , double dvdy, double * x_pnts, double * y_pnts);
