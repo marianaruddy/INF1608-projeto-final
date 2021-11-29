a8774bcacc3327caf38cabdbf3855cf1633c09ce
Mariana Ruddy <31486623+marianaruddy@users.noreply.github.com>
2021-11-28 21:28:21 -0300

    new commit

--- main.c
+++ main.c
@@ -6,18 +6,25 @@
 // clear && gcc -o main projeto.c main.c -lm && ./main
 
 
-double f (double x) {
+double f1 (double x) {
+    return x*x*x-3*x*x+7;
+}
+
+double f_derivada1 (double x) {
+    return 3*x*x-3*x;
+}
+double f2 (double x) {
     return x*x;
 }
 
-double f_derivada (double x) {
+double f_derivada2 (double x) {
     return 2*x;
 }
 
 int main(){
     double x0 = 10.0;
     double tol = 0.0000001;
-    printf("x= %g\n", newton_raphson(x0, f, f_derivada, tol, 50));
+    printf("x= %.9g\n", newton_raphson(x0, f2, f_derivada2, tol, 50));
 
     return 0;
 }
--- projeto.h
+++ projeto.h
@@ -1,3 +1,3 @@
 double newton_raphson(double x0, double f (double x), double f_derivada (double x), double tol, int num_max_iter);
 
-void calc_coord_parametricas (double s, double t, double u (double s, double t), double v (double s, double t), double duds (double s, double t), double dvds (double s, double t), double dudt (double s, double t), double dvdt (double s, double t));
+void calc_coord_parametricas (double x, double y, double u , double v , double dudx , double dvdx , double dudy , double dvdy, double * x_pnts, double * y_pnts);
