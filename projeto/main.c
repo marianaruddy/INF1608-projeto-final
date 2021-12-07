d1c65a98d8f90de264e100a1bf34093369ee1b4e
Mariana Ruddy <31486623+marianaruddy@users.noreply.github.com>
2021-12-07 18:53:32 -0300

    change x_vertices y_vertices

--- projeto/main.c
+++ projeto/main.c
@@ -26,12 +26,12 @@ int main(){
     x_vertices[0] = -2.0;
     x_vertices[1] = 2.0;
     x_vertices[2] = 2.0;
-    x_vertices[3] = -2.0;
+    x_vertices[3] = -20.0;
     
     y_vertices[0] = -2.0;
     y_vertices[1] = -2.0;
     y_vertices[2] = 2.0;
-    y_vertices[3] = 2.0;
+    y_vertices[3] = 20.0;
     
     double x = 1.5;
     double y = 1.5;
@@ -54,23 +54,28 @@ int main(){
     
     calc_coord_parametricas(x, y, s0, t0, x_vertices, y_vertices, tol, 50);
     
-    printf("\ncaso coordenadas parametricas estao no intervalo [−2, 2]:\n");
+    printf("\ncaso coordenadas parametricas estao no intervalo [−2, 2]tb x=y=2.5:\n");
 
     s0 = 0.0;
     t0 = 0.0;
     
     calc_coord_parametricas(2.5, 2.5, s0, t0, x_vertices, y_vertices, tol, 50);
 
-    printf("\ncaso celulas degeneradas:\n");
+    printf("\ncaso celulas degeneradas (dois pontos coincidentes):\n");
 
     x_vertices[1] = x_vertices[0];
+    
+    y_vertices[1] = y_vertices[0];
+    calc_coord_parametricas(x, y, s0, t0, x_vertices, y_vertices, tol, 50);
+
+    printf("\ncaso celulas degeneradas (todos os pontos coincidentes):\n");
+
     x_vertices[2] = x_vertices[0];
     x_vertices[3] = x_vertices[0];
     
-    y_vertices[1] = y_vertices[0];
     y_vertices[2] = y_vertices[0];
     y_vertices[3] = y_vertices[0];
-    calc_coord_parametricas(x, y, s0, t0, x_vertices, y_vertices, tol, 50);
+    // calc_coord_parametricas(x, y, s0, t0, x_vertices, y_vertices, tol, 50);
 
     printf("\ncaso 3d:\n");
 
